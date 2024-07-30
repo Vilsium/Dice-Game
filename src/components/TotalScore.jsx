83dd5a25552f0b222012782f441cc853260c02ed
Vilsium <vilsium28@gmail.com>
2024-07-30 22:05:08 +0530

    recorrecting my code

--- src/components/TotalScore.jsx
+++ src/components/TotalScore.jsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
-const TotalScore = (props) => {
+const TotalScore = ({score}) => {
     return (
         <ScoreContainer>
-            <h1>{props.score}</h1>
+            <h1>{score}</h1>
             <p>Total Score</p>
         </ScoreContainer>
     )
